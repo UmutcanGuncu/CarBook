3ef3044dc94d56929d77222ec56c78373fd526de
UmutcanGuncu <umutcanguncu16@gmail.com>
2024-02-14 21:18:35 +0300

    Kullanıcı temasının js, css, font ve image dosyaları yüklendi

---
+++ Core/CarBook.Application/Dtos/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CarBook.Application.Dtos.ServiceDtos
+{
+	public class ResultServiceDto
+	{
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+    }
+}
+
