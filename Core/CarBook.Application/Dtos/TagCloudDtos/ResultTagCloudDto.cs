c3d6520a182af2c8d3aac2a6ff461b5ef05d77b8
UmutcanGuncu <umutcanguncu16@gmail.com>
2024-03-04 17:00:53 +0300

    TagCloud

---
+++ Core/CarBook.Application/Dtos/TagCloudDtos/ResultTagCloudDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CarBook.Application.Dtos.TagCloudDtos
+{
+	public class ResultTagCloudDto
+	{
+		public Guid id { get; set; }
+		public string title { get; set; }
+		public Guid blogId { get; set; }
+	}
+}
+
--- Presentation/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
+++ Presentation/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98980855f7b8e174952c7b7ec2e258b13a398dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
